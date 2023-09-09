8eccb6e82c130cb3eb12a521485974aa2a278666
xiaomu18 <3466829709@qq.com>
2023-09-09 21:59:00 +0800

    改进获取盘符方法

---
+++ Volcano Services/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.ServiceProcess;
+
+namespace Volcano_Services
+{
+    internal static class Program
+    {
+        /// <summary>
+        /// 应用程序的主入口点。
+        /// </summary>
+        static void Main()
+        {
+            if (!Environment.UserName.Equals("SYSTEM"))
+            {
+                return;
+            }
+
+            ServiceBase[] ServicesToRun;
+            ServicesToRun = new ServiceBase[]
+            {
+                new Service()
+            };
+            ServiceBase.Run(ServicesToRun);
+        }
+    }
+}
---
+++ Volcano Services/Service.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using System.Diagnostics;
+using System.IO;
+using System.Runtime.InteropServices;
+using System.ServiceProcess;
+
+namespace Volcano_Services
+{
+    public partial class Service : ServiceBase
+    {
+        [DllImport("kernel32.dll", SetLastError = true, CharSet = CharSet.Auto)]
+        public static extern bool DeleteVolumeMountPoint(string lpszVolumeMountPoint);
+
+        public Service()
+        {
+            InitializeComponent();
+        }
+
+        protected override void OnStart(string[] args)
+        {
+            string appPath = getAPPPath();
+
+            if (appPath == null)
+            {
+                this.Stop();
+                return;
+            }
+            else
+            {
+                try
+                {
+                    Process process = new Process();
+
+                    process.StartInfo.FileName = appPath;
+                    process.StartInfo.Arguments = "/auto-start";
+                    process.StartInfo.UseShellExecute = false;
+
+                    process.Start();
+                }
+                catch
+                {
+                    Console.WriteLine("An error occurred while automatically starting the volcano background program");
+
+                }
+            }
+
+            this.Stop();
+        }
+
+        protected override void OnStop()
+        {
+        }
+
+        private string getAPPPath()
+        {
+            string driveLetter = Environment.GetEnvironmentVariable("SystemDrive");
+
+            string drivePath = $@"{driveLetter}\";
+
+            bool driveExist = DeleteVolumeMountPoint(drivePath);
+
+            if (driveExist)
+            {
+                string appPath = $@"{drivePath}Program Files\Volcano\volcano_service.exe";
+
+                if (File.Exists(appPath))
+                {
+                    return appPath;
+                } else { return null; }
+            }
+            else
+            {
+                return null;
+            }
+        }
+    }
+}
